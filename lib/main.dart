f7c42a486ae7975887a12602defbafde050da8e3
Jamies-Tran <minhtranquang2197@gmail.com>
2021-06-02 07:30:21 +0700

    MinhTQ_3

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:hci_201/login.dart';
+import 'package:hci_201/views/login.dart';
 import 'package:hci_201/views/new_visitor.dart';
 import 'package:hci_201/views/register.dart';
 
