89a6d64cf508faabd7eb56739c75e440b63026df
Jamies-Tran <minhtranquang2197@gmail.com>
2021-06-03 04:50:32 +0700

    MinhTQ_4

---
+++ lib/widgets/appbar.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+Widget MyAppBar(BuildContext context, String title) {
+  return AppBar(
+    title: Text(
+      title,
+      style: TextStyle(
+        fontSize: 20,
+        fontWeight: FontWeight.bold,
+        fontFamily: 'flower',
+        letterSpacing: 2.0
+      ),
+    ),
+    backgroundColor: Colors.red,
+    centerTitle: true,
+  );
+}
